1eb2c391fbd8c08af9839541a433e0dbc4c762de
hemangsengar <hemang.sengar@gmail.com>
2025-03-17 13:36:04 +0530

    First

---
+++ app.py
@@ -0,0 +1,24 @@
+# Import libraries
+from flask import Flask, redirect, request, render_template, url_for
+
+# Instantiate Flask functionality
+app = Flask(__name__)
+
+# Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
+# Read operation
+@app.route
+
+
+# Create operation
+
+# Update operation
+
+# Delete operation
+
+# Run the Flask app
+    
