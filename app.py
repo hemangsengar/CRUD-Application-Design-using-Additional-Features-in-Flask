b66d39b9b425f4a9a666b1af6aa016cb4a48ec6f
hemangsengar <hemang.sengar@gmail.com>
2025-03-17 18:17:00 +0530

    Done

--- app.py
+++ app.py
@@ -72,7 +72,21 @@ def delete_transaction(transaction_id):
 
     return redirect(url_for("get_transactions"))
 
-# Run the Flask app
+#Search
+
+@app.route("/search", methods = ["GET","POST"])
+def search_transactions():
+    if request.method == "POST":
+        min_amount = float(request.form['min_amount'])
+        max_amount = float(request.form['max_amount'])
+        filtered_transactions = [
+            transaction for transaction in transactions
+            if min_amount <= transaction['amount'] <= max_amount
+        ]
+        return render_template("transactions.html", transactions=filtered_transactions)
     
+    return render_template("search.html")
+
+# Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
